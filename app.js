a68a903a2636c2fa20478ad4af5f4f77e0fcd733
Thenusan Santhirakumar <thenusan@192-168-1-101.tpgi.com.au>
2024-05-02 22:37:33 +1000

    Add changes for 6.2p

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.get("/health", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3040;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
